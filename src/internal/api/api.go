9ce16d9ee95bb3edada7beb518a7c529ac9cee26
Joseph Cáceres <jgcaceres97@gmail.com>
2022-09-10 22:12:56 -0600

    FET: API para el registro de usuarios
    Se agregó el API para el registro de usuarios controlando los errores que pueda presentar.

--- src/internal/api/api.go
+++ src/internal/api/api.go
@@ -1,1 +1,21 @@
 package api
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/jgcaceres97/go-inventory-api/src/internal/service"
+	"github.com/labstack/echo/v4"
+)
+
+type API struct {
+	serv          service.Service
+	dataValidator *validator.Validate
+}
+
+func New(serv service.Service) *API {
+	return &API{serv: serv, dataValidator: validator.New()}
+}
+
+func (a *API) Start(e *echo.Echo, address string) error {
+	a.RegisterRoutes(e)
+	return e.Start(address)
+}
--- src/main.go
+++ src/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jgcaceres97/go-inventory-api/src/database"
+	"github.com/jgcaceres97/go-inventory-api/src/internal/api"
 	"github.com/jgcaceres97/go-inventory-api/src/internal/repository"
 	"github.com/jgcaceres97/go-inventory-api/src/internal/service"
 	"github.com/jgcaceres97/go-inventory-api/src/settings"
+	"github.com/labstack/echo/v4"
 	"go.uber.org/fx"
 )
 
@@ -18,9 +21,25 @@ func main() {
 			database.New,
 			repository.New,
 			service.New,
+			api.New,
+			echo.New,
 		),
-		fx.Invoke(),
+		fx.Invoke(setLifeCycle),
 	)
 
 	app.Run()
+}
+
+func setLifeCycle(lc fx.Lifecycle, a *api.API, s *settings.Settings, e *echo.Echo) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			address := fmt.Sprintf(":%s", s.Port)
+			go a.Start(e, address)
+
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return nil
+		},
+	})
 }
